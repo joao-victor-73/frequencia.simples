9700f689b4099d08ac7def34c8deec0901772b87
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-03-25 22:19:26 -0300

    primeiros passos concluidos
    Fiz as configurações iniciais, preparei todas as tabelas e também o SQLAlchemy, além de mostrar na tela uma lista com os nomes.

---
+++ app.py
@@ -0,0 +1,110 @@
+from flask import Flask, render_template
+import os
+import dotenv
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Configuração do Banco de Dados (Essas configs vem do arquivo .env)
+dotenv.load_dotenv()
+
+host = os.environ['MYSQL_HOST']
+user = os.environ['MYSQL_USER']
+password = os.environ['MYSQL_PASSWORD']
+database = os.environ['MYSQL_DATABASE']
+
+# Instanciando a aplicação e o SQLAlchemy
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+print("Banco de dados Conectado!")
+
+db = SQLAlchemy(app)
+
+
+# Classes / Models para as TABELAS
+
+class Catequistas(db.Model):
+    __tablename__ = 'catequistas'
+
+    id_catequista = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    data_nascimento = db.Column(db.Date, nullable=False)
+    grupo = db.Column(db.String(100), default='Nao Especificado')
+    nivel = db.Column(db.Enum('coordenador', 'catequista'), nullable=False, default='catequista')
+
+    # (1:N - One-to-Many) UM catequista pode ter VÁRIOS crismandos
+    crismandos = db.relationship('Crismandos', backref='cat', lazy=True)
+
+    # (1:1 - One-to-One)
+    login = db.relationship('Usuarios', uselist=False, backref='usuario')
+
+
+
+class Crismandos(db.Model):
+    __tablename__ = 'crismandos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    nome_mae = db.Column(db.String(100), nullable=False)
+    nome_pai = db.Column(db.String(100), nullable=False, default='Nao mencionado')
+    data_nascimento = db.Column(db.Date, nullable=False)
+    endereco = db.Column(db.String(200), nullable=False)
+    tel1 = db.Column(db.String(15), nullable=False)
+    tel2 = db.Column(db.String(15))
+    batismo = db.Column(db.Enum('sim', 'nao'), default='nao')
+    eucaristia = db.Column(db.Enum('sim', 'nao'), default='nao')
+    status_crismando = db.Column(db.Enum('ativo', 'desistente'), default='ativo')
+
+    # Chave Estrangeira (ForeignKey)
+    fk_id_catequista = db.Column(db.Integer, db.ForeignKey('catequistas.id_catequista'))
+
+    # (1:N) UM crismando pode ter VÁRIAS frequências
+    frequencias = db.relationship('Frequencias', backref='freq')
+
+
+class Frequencias(db.Model):
+    __tablename__ = 'frequencias'
+
+    id_freq = db.Column(db.Integer, primary_key=True)
+    data_frequencia = db.Column(db.Date, nullable=False)
+    status_frequencia = db.Column(
+        db.Enum('presente', 'falta', 'justificada'), default='falta')
+    observacao = db.Column(db.Text)
+
+    # Chave Estrangeira
+    fk_id_crismando = db.Column(db.Integer, db.ForeignKey('crismandos.id'), nullable=False)
+
+
+
+class Usuarios(db.Model):
+    __tablename__ = 'usuarios'
+
+    id_usuario = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    senha_hash = db.Column(db.String(255), nullable=False)
+
+    # Chave Estrangeira
+    fk_id_catequista = db.Column(db.Integer, db.ForeignKey('catequistas.id_catequista'), nullable=False)
+
+
+# Rota de começo
+@app.route("/", methods=["GET"])
+def index():
+    lista_crismandos = Crismandos.query.all()
+    return render_template('index.html', lista_crismandos = lista_crismandos)
+
+
+# Rota para registrar presença
+@app.route("/registrar_presenca", methods=["POST"])
+def registrar_presenca():
+    pass
+
+# Rota para consultar frequência
+@app.route("/frequencia", methods=["GET"])
+def listar_frequencia():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db.sql
@@ -0,0 +1,113 @@
+-- CRIANDO A BASE DE DADOS
+CREATE DATABASE IF NOT EXISTS crisma;
+USE crisma;
+
+
+-- UM crismando pode ter apenas UM catequista (1:1)
+
+
+-- CRIANDO AS TABELAS NECESSÁRIAS
+CREATE TABLE IF NOT EXISTS catequistas (
+	id_catequista INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    grupo VARCHAR(100) DEFAULT "Nao Especificado",
+    nivel ENUM('coordenador', 'catequista') DEFAULT 'catequista',
+    status_informacao BOOLEAN DEFAULT 1
+);
+
+-- ALTER TABLE catequistas
+-- ADD COLUMN status_informacao BOOLEAN DEFAULT 1;
+
+
+CREATE TABLE IF NOT EXISTS crismandos (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    nome_mae VARCHAR(100) NOT NULL,
+    nome_pai VARCHAR(100) DEFAULT 'Não mencionado',
+    data_nascimento DATE NOT NULL,
+    endereco VARCHAR(200) NOT NULL,
+    tel1 VARCHAR(15) NOT NULL,
+    tel2 VARCHAR(15),
+    batismo ENUM('sim', 'nao') DEFAULT 'nao',
+    eucaristia ENUM('sim', 'nao') DEFAULT 'nao',
+    status_crismando ENUM('ativo', 'desistente') DEFAULT 'ativo',
+    fk_id_catequista INT,
+    
+    CONSTRAINT fk_catequista_crismando
+    FOREIGN KEY (fk_id_catequista) REFERENCES catequistas(id_catequista)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+-- UM crismando pode ter VARIAS frequencias (1:N)
+
+CREATE TABLE IF NOT EXISTS frequencias (
+    id_freq INT PRIMARY KEY AUTO_INCREMENT,
+    fk_id_crismando INT NOT NULL,
+    data_frequencia DATE NOT NULL,
+    status_frequencia ENUM('presente', 'falta', 'justificada') DEFAULT 'falta',
+    observacao TEXT NULL,
+
+    CONSTRAINT fk_crismando_frequencia
+    FOREIGN KEY (fk_id_crismando) REFERENCES crismandos(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+
+-- ALTER TABLE frequencias
+-- RENAME COLUMN status TO status_frequencia;
+
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL,
+    fk_id_catequista INT UNIQUE NOT NULL,
+
+    CONSTRAINT fk_usuario_catequista
+    FOREIGN KEY (fk_id_catequista) REFERENCES catequistas(id_catequista)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+-- ALGUNS INSERT's para servir só de exemplos
+INSERT INTO catequistas (nome, data_nascimento, grupo, nivel)
+VALUES 
+('João da Silva', '1985-06-15', 'Grupo A', 'catequista'),
+('Maria Oliveira', '1978-11-22', 'Grupo B', 'coordenador');
+
+
+INSERT INTO crismandos (nome, nome_mae, nome_pai, data_nascimento, endereco, tel1, tel2, batismo, eucaristia, status_crismando, fk_id_catequista)
+VALUES 
+('Lucas Pereira', 'Ana Pereira', 'Carlos Pereira', '2005-04-10', 'Rua das Flores, 123', '999999999', '988888888', 'sim', 'sim', 'ativo', 1),
+('Fernanda Souza', 'Cláudia Souza', 'Roberto Souza', '2006-08-25', 'Avenida Central, 456', '977777777', NULL, 'sim', 'nao', 'ativo', 2),
+('Pedro Lima', 'Juliana Lima', 'Não mencionado', '2004-12-15', 'Rua do Sol, 789', '966666666', '955555555', 'nao', 'sim', 'desistente', 1);
+
+
+INSERT INTO frequencias (fk_id_crismando, data_frequencia, status_frequencia, observacao)
+VALUES 
+(1, '2025-03-01', 'presente', 'Chegou cedo, participou ativamente'),
+(2, '2025-03-01', 'justificada', 'Faltou devido a viagem com a família'),
+(3, '2025-03-01', 'falta', 'Não compareceu à catequese'),
+(1, '2025-03-08', 'presente', 'Participou de todas as atividades');
+
+
+-- TESTANDO UMAS COISAS
+
+
+SELECT * FROM catequistas;
+SELECT * FROM crismandos;
+SELECT * FROM frequencias;
+
+SELECT
+	c.id,
+	c.nome,
+    COUNT(f.status_frequencia) AS quant_frequencia
+FROM frequencias f
+INNER JOIN crismandos c ON f.fk_id_crismando = c.id
+WHERE status_frequencia = 'presente'
+GROUP BY c.id, c.nome;
