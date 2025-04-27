9f29a9ed7308d465086426977aa8d8071290b7bb
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-04-27 12:49:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,26 +9,30 @@ from routes import register_routes
 login_manager = LoginManager()
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-db.init_app(app)
+    db.init_app(app)
 
-login_manager = LoginManager(app)
-login_manager.init_app(app)
-login_manager.login_view = 'auth.login'
-login_manager.login_message = 'Por favor, faça login para continuar.'
-login_manager.login_message_category = 'warning'
+    login_manager = LoginManager(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Por favor, faça login para continuar.'
+    login_manager.login_message_category = 'warning'
 
-@login_manager.user_loader
-def load_user(user_id):
-    return Usuarios.query.get(int(user_id))
-
-
-with app.app_context():
-    from models import Usuarios
-    from utils.decorators import coordenador_required
-    register_routes(app)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Usuarios.query.get(int(user_id))
 
 
-app.run(debug=True)
+    with app.app_context():
+        from models import Usuarios
+        from utils.decorators import coordenador_required
+        register_routes(app)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
