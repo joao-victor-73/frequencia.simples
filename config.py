1305ad2d2a8d19065f54fff55765e428d43a516e
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-04-27 13:53:38 -0300

    edit bd config

--- config.py
+++ config.py
@@ -9,9 +9,12 @@ password = os.environ['MYSQL_PASSWORD']
 database = os.environ['MYSQL_DATABASE']
 secret_key = os.environ['SECRET_KEY']
 
+# Usar na hospedagem mysql://root:RQuWPWiVzhgzwiiVbvgzKFcEyEPyCrsJ@maglev.proxy.rlwy.net:54567/railway
+
 
 class Config:
     SECRET_KEY = secret_key
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+    # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}/{database}"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:RQuWPWiVzhgzwiiVbvgzKFcEyEPyCrsJ@maglev.proxy.rlwy.net:54567/railway"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_DURATION = os.environ['REMEMBER_COOKIE_DURATION']
