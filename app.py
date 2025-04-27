387092ceb404cc0ebd8dc2524015e142ea3f3990
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-04-27 12:44:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,29 +9,26 @@ from routes import register_routes
 login_manager = LoginManager()
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    db.init_app(app)
+db.init_app(app)
 
-    login_manager = LoginManager(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
-    login_manager.login_message = 'Por favor, faça login para continuar.'
-    login_manager.login_message_category = 'warning'
+login_manager = LoginManager(app)
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+login_manager.login_message = 'Por favor, faça login para continuar.'
+login_manager.login_message_category = 'warning'
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return Usuarios.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuarios.query.get(int(user_id))
 
 
-    with app.app_context():
-        from models import Usuarios
-        from utils.decorators import coordenador_required
-        register_routes(app)
+with app.app_context():
+    from models import Usuarios
+    from utils.decorators import coordenador_required
+    register_routes(app)
 
-    return app
 
-app = create_app()
 app.run(debug=True)
