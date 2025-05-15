37a6fa4ed983e38dbf5ea2e0ea2698e735663336
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-05-15 19:52:38 -0300

    update config

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from flask_mail import Mail, Message
 
 load_dotenv()
 
@@ -9,15 +8,6 @@ user = os.environ['MYSQL_USER']
 password = os.environ['MYSQL_PASSWORD']
 database = os.environ['MYSQL_DATABASE']
 secret_key = os.environ['SECRET_KEY']
-
-
-# E-MAIL
-mail_server = os.environ['MAIL_SERVER']
-mail_port = os.environ['MAIL_PORT']
-mail_use_tls = os.environ['MAIL_USE_TLS']
-mail_username = os.environ['MAIL_USERNAME']
-mail_password = os.environ['MAIL_PASSWORD']
-mail_default_sender = os.environ['MAIL_DEFAULT_SENDER']
 
 
 class Config:
