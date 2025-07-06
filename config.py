f563ab45d0ea78cc277e7fc2525a3d9a8c40f53e
João Victor <121986042+joao-victor-73@users.noreply.github.com>
2025-07-06 19:11:27 -0300

    resolvendo problema de conexão

--- config.py
+++ config.py
@@ -15,6 +15,12 @@ port = os.environ['MYSQL_PORT']
 
 class Config:
     SECRET_KEY = secret_key
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?connect_timeout=30"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_DURATION = os.environ['REMEMBER_COOKIE_DURATION']
+
+    # Evitar erro de lost connection
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,    # testa se a conexão ainda está viva
+        "pool_recycle": 280       # reinicia conexão a cada 280s
+    }
